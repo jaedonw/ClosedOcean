dfbb138a28a9eb90fc4984505d244c041d808925
Jaedon Wong <jaedonw79@gmail.com>
2023-11-08 15:46:31 -0500

    checkpoint 2

---
+++ contracts/AuctionCoin.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9; 
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract AuctionCoin is ERC20 {
+    
+}
